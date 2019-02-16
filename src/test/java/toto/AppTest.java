665f5d51538f13067471588df9d8f982edb85184
Daniel Segovia <segovia-mera@et.esiea.fr>
2019-02-16 04:35:59 -0600

    support des tests

--- src/test/java/toto/AppTest.java
+++ src/test/java/toto/AppTest.java
@@ -1,5 +1,7 @@
 package toto;
 
+
+import static org.assertj.core.api.Assertions.*;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -8,31 +10,11 @@ import junit.framework.TestSuite;
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+    
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+    @Test
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    void TesT(){
+	    assertThat("halo"),isEqualTo("halo");
     }
 }
