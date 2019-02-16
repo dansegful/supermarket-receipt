8c47b66d52c759f3db596aba9040d1a8e054fec7
Daniel Segovia <segovia-mera@et.esiea.fr>
2019-02-16 06:11:58 -0600

    _

---
+++ titi/src/test/java/toto/AppTest.java
@@ -0,0 +1,20 @@
+package toto;
+
+
+import static org.assertj.core.api.Assertions.*;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    
+{
+    @Test
+
+    void TesT(){
+	    assertThat("halo"),isEqualTo("halo");
+    }
+}
